c9d21c2768b5e9d1126ac3b19cdc929cb333092a
saniev96 <73014352+saniev96@users.noreply.github.com>
2020-12-11 12:12:41 +0300

    js

---
+++ homeworkjs.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// 1. Задать высоту, длину и ширину прямоугольного параллепипеда
+let width = a;
+let height = b;
+let lenght = c;
+let square = 2 *(ab + bc + ac );
+console.log();
+
+// 2. В переенной n хранится двузначное число.
+let n = 32;
+let first = n / 10;
+let second = n % 10;
+let sum = first + second;
+console.log();
+
+// 3. Поменять значение двух переменных, не используя третью. 
+let a = 1;
+let b = 2;
+a = a + b;
+a = a - b;
+b = a - b;
+console.log(a);
+console.log(b);
+
